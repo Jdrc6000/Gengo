8fd0e3cebaa52433ab23217e9d6d1a966d50833e
Joshua Carter <joshuacarter@Mac.Home>
2026-02-11 22:03:21 +0000

    Simple reg alloc in VM - needs work (line 21)

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from src.ir.generator import IRGenerator
 from src.runtime.vm import VM
 
 code = """
-x = 5 - 5
-print(x)
-if x == 10:
-    print("yes")
-else:
-    print("no")
+x = 4 + 3
+print(2 + x)
 """
 
 lexer = Lexer(code)
@@ -29,7 +25,7 @@ ir_generator = IRGenerator()
 ir_generator.generate(tree)
 ir_generator.ir.dump()
 
-vm = VM()
+vm = VM(num_regs=6)
 vm.run(ir_generator.ir.code)
 for index, reg in enumerate(vm.regs):
     if reg: print(f"r{index} {reg}")
--- src/runtime/vm.py
+++ src/runtime/vm.py
@@ -3,10 +3,24 @@
 # past-josh: PLEASE FOR THE LOVE OF GOD REFACTOR TO REGISTER-BASED!!
 # future-josh: your wish is my command
 class VM:
-    def __init__(self):
-        self.regs = [None] * 1024
+    def __init__(self, num_regs):
+        self.num_regs = num_regs
+        self.regs = [None] * self.num_regs
+        self.free_regs = list(range(self.num_regs))
         self.vars = {}
         self.ip = 0 # instruction pointer
+    
+    def alloc_reg(self):
+        print("allocating reg")
+        if not self.free_regs:
+            raise RuntimeError("Out of registers")
+        return self.free_regs.pop(0)
+    
+    def free_reg(self, reg):
+        print(f"freeing reg: {reg} ({self.regs[reg]})")
+        if True: #reg not in self.free_regs:
+            self.free_regs.insert(0, reg)
+            self.regs[reg] = None
     
     def run(self, code):
         self.ip = 0
@@ -14,6 +28,15 @@ class VM:
         while self.ip < len(code):
             instr = code[self.ip]
             op, a, b, c = instr.op, instr.a, instr.b, instr.c
+            
+            if op in ("LOAD_CONST", "LOAD_VAR"):
+                if a is None:
+                    a = self.alloc_reg()
+                    instr.a = a
+            elif op in ("ADD","SUB","MUL","DIV","POW","NEG","NOT","EQ","NE","LT","GT","LE","GE"):
+                if a is None:
+                    a = self.alloc_reg()
+                    instr.a = a
             
             if op == "LOAD_CONST":
                 self.regs[a] = b
@@ -23,9 +46,11 @@ class VM:
             
             elif op == "STORE_VAR":
                 self.vars[a] = self.regs[b]
+                self.free_reg(b)
             
             elif op == "PRINT":
                 print(self.regs[a])
+                self.free_reg(a)
             
             elif op == "JUMP":
                 self.ip = a
@@ -35,52 +60,81 @@ class VM:
                 if self.regs[a]:
                     self.ip = b
                     continue
+                self.free_reg(a)
             
             elif op == "JUMP_IF_FALSE":
                 if not self.regs[a]:
                     self.ip = b
                     continue
+                self.free_reg(a)
+            
+            elif op == "MOVE":
+                self.regs[a] = self.regs[b]
             
             # arithmetic
             elif op == "ADD":
                 self.regs[a] = self.regs[b] + self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "SUB":
                 self.regs[a] = self.regs[b] - self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "MUL":
                 self.regs[a] = self.regs[b] * self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "DIV":
                 self.regs[a] = self.regs[b] / self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "POW":
                 self.regs[a] = self.regs[b] ** self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "NEG":
                 self.regs[a] = -self.regs[b]
+                self.free_reg(b)
 
             elif op == "NOT":
                 self.regs[a] = not self.regs[b]
+                self.free_reg(b)
 
             # comparisons
             elif op == "EQ":
                 self.regs[a] = self.regs[b] == self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "NE":
                 self.regs[a] = self.regs[b] != self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "LT":
                 self.regs[a] = self.regs[b] < self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "GT":
                 self.regs[a] = self.regs[b] > self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "LE":
                 self.regs[a] = self.regs[b] <= self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             elif op == "GE":
                 self.regs[a] = self.regs[b] >= self.regs[c]
+                self.free_reg(b)
+                self.free_reg(c)
 
             else:
                 raise RuntimeError(f"Unknown opcode {op}")
